bf8a582525f53da18e6557420930060db40ebb11
Rene <Rene@AU333500>
2020-03-11 10:58:10 +0100

    Added main window components

---
+++ GUI_assignment1_gr23/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_assignment1_gr23
+{
+    class MainWindowViewModel
+    {
+
+    }
+}
