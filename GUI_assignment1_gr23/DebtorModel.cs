cabcf3f7ca6d15c4e3d333a27588f3b8f8251f1d
Rene <Rene@AU333500>
2020-03-11 11:06:38 +0100

    Added DebtorModel class

---
+++ GUI_assignment1_gr23/DebtorModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_assignment1_gr23
+{
+    public class DebtorModel
+    {
+        public DebtorModel()
+        {
+            DebtorObsList = new ObservableCollection<Deptor>();
+        }
+
+        public ObservableCollection<Deptor> DebtorObsList { get; set; }
+    }
+}
