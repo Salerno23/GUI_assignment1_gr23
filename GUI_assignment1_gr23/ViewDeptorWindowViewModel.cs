f8ea1c2f0d92c66dc7867af0219edb41b90ed5ca
Stine F. Gravesen <au553805@post.au.dk>
2020-03-11 09:18:57 +0100

    Added View Deptor Window and View Model

---
+++ GUI_assignment1_gr23/ViewDeptorWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_assignment1_gr23
+{
+    class ViewDeptorWindowViewModel
+    {
+    }
+}
