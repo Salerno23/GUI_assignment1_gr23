d7f8fe5aff9bd5839bfdb3770fadb0b4ce7e92c2
Stine F. Gravesen <au553805@post.au.dk>
2020-03-13 08:33:37 +0100

    Desing and function changes in View Deptor

--- GUI_assignment1_gr23/ViewDeptorWindowViewModel.cs
+++ GUI_assignment1_gr23/ViewDeptorWindowViewModel.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace GUI_assignment1_gr23
 {
-    class ViewDeptorWindowViewModel
+    class ViewDeptorWindowViewModel : BindableBase
     {
+
+        public void AddValue()
+        {
+
+        }
+
+        public void CloseWindow()
+        {
+            
+        }
+
+
+        ICommand addValue;
+
+        public ICommand AddValueCommand
+        {
+            get
+            {
+                return addValue ?? (addValue = new DelegateCommand(AddValue));
+            }
+        }
+
+        ICommand closeWindow;
+
+        public ICommand CloseWindowCommand
+        {
+            get
+            {
+                return closeWindow ?? (closeWindow = new DelegateCommand(CloseWindow));
+            }
+        }
+
     }
 }
