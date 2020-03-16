daa02c9663952f555d6ec18d4a66d88c29e95450
Rene Street <20112809@post.au.dk>
2020-03-16 23:25:38 +0100

    Dummy data no longer required

--- TestDummyData.sql
+++ TestDummyData.sql
@@ -1,31 +1,32 @@
-﻿use [debtbook]
-GO
+﻿-- Not necessary, testing purposes only
+--use [debtbook]
+--GO
 
-INSERT INTO Debtors VALUES
-(
-	1,
-	'Henrik',
-	300
-)
+--INSERT INTO Debtors VALUES
+--(
+--	1,
+--	'Henrik',
+--	300
+--)
 
-INSERT INTO Debts VALUES
-(
-	1,
-	GETDATE(),
-	300
-)
+--INSERT INTO Debts VALUES
+--(
+--	1,
+--	GETDATE(),
+--	300
+--)
 
 
-INSERT INTO Debtors VALUES
-(
-	2,
-	'Michael',
-	-600
-)
+--INSERT INTO Debtors VALUES
+--(
+--	2,
+--	'Michael',
+--	-600
+--)
 
-INSERT INTO Debts VALUES
-(
-	2,
-	GETDATE(),
-	-600
-)
+--INSERT INTO Debts VALUES
+--(
+--	2,
+--	GETDATE(),
+--	-600
+--)
