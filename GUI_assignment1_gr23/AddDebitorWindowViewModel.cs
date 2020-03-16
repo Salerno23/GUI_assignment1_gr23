e6bea3ae065491ed09453c695b44cf18294e61a5
Dodi <201808180@post.au.dk>
2020-03-16 11:16:38 +0100

    Changes to AddDebitorWindowViewModel

--- GUI_assignment1_gr23/AddDebitorWindowViewModel.cs
+++ GUI_assignment1_gr23/AddDebitorWindowViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.Security.RightsManagement;
 
 namespace GUI_assignment1_gr23
 {
-    class AddDebitorWindowViewModel
+    public class AddDebitorWindowViewModel 
     {
+
     }
 }
